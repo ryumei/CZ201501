26e4ed248b02c60b26a65d2daddce2da3ea5c0f1
NAKAJIMA Takaaki <nakajima_t@worksap.co.jp>
2015-01-05 16:06:53 +0900

    add description field

--- issue_reporter.py
+++ issue_reporter.py
@@ -13,20 +13,20 @@ parser.add_option('-u', '--url', dest='url', help='Endpoint url of a redmine')
 parser.add_option('-k', '--key', dest='key', help='API Access  Key')
 parser.add_option('-s', '--subject', dest='subject', help='Subject of a reporting issue')
 parser.add_option('-p', '--project_id', dest='project_id', help='Project ID of the issue')
+parser.add_option('-d', '--description', dest='description', help='Description of the issue')
 (options, args) = parser.parse_args()
-
-print options
-
-print options.url
 
 redmine = Redmine(options.url, key=options.key)
 issue = redmine.issue.create(project_id=options.project_id, 
                              subject=options.subject)
+if options.description is None:
+    issue.description = u"This ticket was created by issue_reporter.py"
+else:
+    issue.description = options.description
 issue.notes = u"""
-このチケットは Zabbix から自動登録されました。
+このチケットは自動登録されました。
 確認、対応をお願いします。
 """
 
 issue.save()
 
-print issue.id
